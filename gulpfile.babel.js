e491b56fc93d5b1cca238025ea09e0d35fda01a6
SergBozhko <stwnsn@gmail.com>
2016-05-18 12:52:05 +0300

    Added catalog page

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -57,10 +57,12 @@ gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    .pipe($.if('', $.cssnano()))
+    .pipe($.if('', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
+  //.pipe($.if('*.css', $.cssnano()))
+  //.pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
